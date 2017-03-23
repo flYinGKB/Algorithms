2886d6dd598aece954dafa94d93a2e5fa3269717
flYinGKB <flYinGKB@users.noreply.github.com>
2017-03-23 12:57:17 -0400

    Update p001.py
    add solution from 九章算法

--- p001.py
+++ p001.py
@@ -1,5 +1,32 @@
 # Leetcode problem 001
-
-
-danny
 # Two sum
+/**
+ * 本代码由九章算法编辑提供。版权所有，转发请注明出处。
+ * - 九章算法致力于帮助更多中国人找到好的工作，教师团队均来自硅谷和国内的一线大公司在职工程师。
+ * - 现有的面试培训课程包括：九章算法班，系统设计班，算法强化班，Java入门与基础算法班，Android 项目实战班，Big Data 项目实战班，
+ * - 更多详情请见官方网站：http://www.jiuzhang.com/?source=code
+ */
+
+public class Solution {
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1 + 1, index2 + 1] (index1 < index2)
+         numbers=[2, 7, 11, 15],  target=9
+         return [1, 2]
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.get(numbers[i]) != null) {
+                int[] result = {map.get(numbers[i]) + 1, i + 1};
+                return result;
+            }
+            map.put(target - numbers[i], i);
+        }
+        
+        int[] result = {};
+        return result;
+    }
+}
